ed5d76476b61c093a4c50b965278ead64b034e4f
Farid <Farid@158.37.74.56>
2017-08-23 13:37:27 +0200

    lever

--- src/no/hvl/dat100/Kalkulator.java
+++ src/no/hvl/dat100/Kalkulator.java
@@ -2,8 +2,8 @@ package no.hvl.dat100;
 
 public class Kalkulator {
 
-	public static int add(int a, int b) {
-	return a+b;
+	public static int add(int x, int y) {
+	return x+y;
 	}
 
 	public static int sub(int a, int b) {
